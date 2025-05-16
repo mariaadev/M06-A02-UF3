fad47c273903ade4a0ad6b92d3f57b0594675447
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 17:27:47 +0200

    Afegit StudentService

--- act2/src/main/java/com/accesadades/act2/Serveis/StudentService.java
+++ act2/src/main/java/com/accesadades/act2/Serveis/StudentService.java
@@ -1,6 +1,7 @@
 package com.accesadades.act2.Serveis;
 
 import com.accesadades.Modal.Student;
+import com.accesadades.act2.DTO.StudentDTO;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,6 +10,7 @@ public interface StudentService {
     Mono<Student> save(); 
     Mono<Student> findById(String id); 
     Flux<Student> findAll(); 
-    
+    Mono<Student> update(StudentDTO nom_dto);
+    Mono<Void> delete(String id);
 
 }
