3170645f838fcc049837c266fd34098b29d30237
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 16:55:12 +0200

    Initial commit

---
+++ act2/src/main/java/com/accesadades/act2/Act2Application.java
@@ -0,0 +1,13 @@
+package com.accesadades.act2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act2Application.class, args);
+	}
+
+}
---
+++ act2/src/test/java/com/accesadades/act2/Act2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesadades.act2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
