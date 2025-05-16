ca39564572589745e4ebc4706a3d204b32031642
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 17:55:28 +0200

    Afegits endpoints update i delete

--- act2/src/main/java/com/accesadades/act2/Controllers/StudentController.java
+++ act2/src/main/java/com/accesadades/act2/Controllers/StudentController.java
@@ -1,14 +1,20 @@
-package com.accesadades.act2.Controlllers;
+package com.accesadades.act2.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accesadades.act2.DTO.StudentDTO;
+import com.accesadades.act2.Model.Student;
 import com.accesadades.act2.Serveis.StudentService;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -33,9 +39,13 @@ public class StudentController {
     @GetMapping("/all")
 
     @PutMapping("/update") 
-
+    public Mono<Student> updateStudent(@RequestBody StudentDTO dto) {
+        return studentService.update(dto);
+    }
     @DeleteMapping("/{Id}")
-
+    public Mono<Void> deleteStudent(@PathVariable String id) {
+        return studentService.delete(id);
+    }
 
 
 }
