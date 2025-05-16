8a83c58bce46cf6fada6cfa49fa6cdcfdfd9aaab
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 17:17:18 +0200

    Afegit Mapper

---
+++ act2/src/main/java/com/accesadades/act2/Mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.accesadades.act2.Mapper;
+
+import java.util.List;
+
+import org.mapstruct.*;
+
+import com.accesadades.Modal.Student;
+import com.accesadades.act2.DTO.StudentDTO;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface StudentMapper {
+    StudentDTO studentToStudentDTO(Student student);
+    Student studenDTOtToStudent(StudentDTO studentDTO);
+
+    List<StudentDTO> studentsToStudentDTOs(List<Student> students);
+    List<Student> studentsDTOsToStudents(List<StudentDTO> StudentDTOs);
+}
