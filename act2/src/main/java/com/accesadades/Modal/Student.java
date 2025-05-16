be434170e69b0954089032dfb5d082128df24d97
Helenagapr <helenagp05@gmail.com>
2025-05-16 17:12:33 +0200

    añadido el modal

---
+++ act2/src/main/java/com/accesadades/Modal/Student.java
@@ -0,0 +1,30 @@
+package com.accesadades.Modal;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Document(collection="students")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student implements Serializable {
+	@Id
+    private String id;
+    private String name;
+    private String lastname1;
+    private String lastname2;
+    private String gender;
+    private String email;
+    private String phone;
+    private int birth_year;
+    private String DNI;
+}
