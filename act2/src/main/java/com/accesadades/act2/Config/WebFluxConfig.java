da7db4650d2d520b73f9218f9dfafb65942c3fa0
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 17:02:53 +0200

    Afegides classes OpenApiConfig i WebFluxConfig

---
+++ act2/src/main/java/com/accesadades/act2/Config/WebFluxConfig.java
@@ -0,0 +1,11 @@
+package com.accesadades.act2.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
+}
