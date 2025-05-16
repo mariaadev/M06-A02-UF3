84d35ab278c9b23a97f7e8690615b31948464985
Maria Amzil <mini.mar@hotmail.com>
2025-05-16 17:08:51 +0200

    Afegit DTO

---
+++ act2/src/main/java/com/accesadades/act2/DTO/StudentDTO.java
@@ -0,0 +1,13 @@
+package com.accesadades.act2.DTO;
+
+public record StudentDTO (
+    String name,
+    String lastname1,
+    String lastname2,
+    String gender,
+    String email,
+    String phone,
+    int birth_year,
+    String dni
+) {}
+
